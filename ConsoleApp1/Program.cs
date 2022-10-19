d36a06db6062c76882a0cb5685f3123a45509820
Tománek Filip <tomanek.fi.2022@skola.ssps.cz>
2022-10-19 11:38:09 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong num = Convert.ToUInt64(Console.ReadLine());
+            bool prime = true;
+
+
+            if (num % 2 == 0 && num > 2)
+            {
+                prime = ;
+            }
+            else
+            {
+                int boundary = (int)Math.Sqrt(num);
+                for (int i = 3; i < boundary;  i+= 2)
+                {
+                    if (num % Convert.ToUInt6(i) == 0)
+                    {
+                        prime = false;
+                    }
+
+                }
+            }
+
+
+
+            
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
